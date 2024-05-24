6c2cfcf8537b5d6a1899f89defbba192080142e3
Silver <garrypedrosa.31@gmail.com>
2024-05-24 12:25:05 +0800

    For User Stories 1

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Student;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -13,11 +14,32 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
         User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+            'name' => 'Garry Caber',
+            'email' => 'garrypedrosa.31@gmail.com',
+        ]);
+        
+        Student::factory(10)->create();
+        Student::factory()->create([
+            'firstname' => 'Garry Caber', 
+            'lastname' => 'Caber',
+            'birthdate' => '2003-09-18',
+            'sex' => 'MALE',
+            'address' => 'Brgy. Tinaogan Basey, Samar',
+            'year' => 3,
+            'course' => 'BSIT',
+            'section' => 'B',
+        ]);
+
+        Student::factory()->create([
+            'firstname' => 'Juan', 
+            'lastname' => 'Ford',
+            'birthdate' => '2001-05-07',
+            'sex' => 'MALE',
+            'address' => 'Brgy. San. Antonio Basey, Samar',
+            'year' => 3,
+            'course' => 'BSIT',
+            'section' => 'A',
         ]);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,18 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\StudentController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+//STUDENT
+//GET
+Route::get('/students', [StudentController::class, 'index']);
+//POST
+Route::post('/students', [StudentController::class, 'register']);
+//GET (SPECIFIC)
+Route::get('/students/{id}', [StudentController::class, 'find']);
+//PATCH
+Route::patch('/students/{id}', [StudentController::class, 'update']);
